1e29ee9e7c079871fbe54c4e35c1995d96704431
Mell0w_Fell0w <mccoopton@gmail.com>
2024-03-08 19:59:46 -0700

    Welcome func added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+function Welcome() {
+  return <h1>College Basketball Teams</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      {/* The function has to be capitalized to work */}
+      <Welcome />
     </div>
   );
 }
